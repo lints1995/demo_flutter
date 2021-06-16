602e10f5c7035c17bca92389341b136ecfc433fd
lints <LinTianSheng@newhope.cn>
2021-06-16 19:25:48 +0800

    修改包名

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo_app/component/CustomButton.dart';
+import 'package:demo_flutter/component/CustomButton.dart';
 // 页面
 import 'views/base.dart';
 
